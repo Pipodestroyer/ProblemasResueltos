330ab59f902f69002a8ae0be28d33a628e241963
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2026-08-16 14:02:42 -0500

    Amazing things happening in happening town.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,20 @@
-void main() {
+public class Main {
+    public static double myPow(double x, int n){
+        double base = x;
+        double respuesta = 1;
+        for (int i = 1; i<=n; i++){
+            if ( base < -100 || base > 100){
+                break;
+            }
+            respuesta = respuesta * base;
+        }
 
+        return respuesta;
+    }
+
+    public static void main(String[] args){
+        double result = myPow(102, 64);
+
+        IO.println(result);
+    }
 }
