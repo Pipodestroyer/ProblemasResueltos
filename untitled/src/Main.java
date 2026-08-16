39f9ad87d266528dc5c5f155c3ad9dfab4efabcd
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2026-08-16 14:53:37 -0500

    this is my thing

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,29 +1,28 @@
-public class Main {
-    public static double myPow(double x, int n){
 
-        double respuesta = 1;
-        int ciclos = Math.abs(n);
+public static double myPow(double x, int n){
 
-        if ( x < -100 || x > 100 || x == 0 && n < 0){
-            return 0;
-        }
+    double respuesta = 1;
+    int ciclos = Math.abs(n);
 
-        for (int i = 1; i<= ciclos; i++){
-            if (0 > n) {
-                respuesta = respuesta/x;
-            } else {
-                respuesta = respuesta * x;
-            }
-        }
-
-        return respuesta;
-
+    if ( x < -100 || x > 100 || x == 0 && n < 0){
+        return 0;
     }
 
-    public static void main(String[] args){
-
-        double result = myPow(2, -2);
-
-        IO.println(result);
+    for (int i = 1; i<= ciclos; i++){
+        if (0 > n) {
+            respuesta = respuesta/x;
+        } else {
+            respuesta = respuesta * x;
+        }
     }
+
+    return respuesta;
+
+}
+
+ void main(){
+
+    double result = myPow(2, -2);
+
+    IO.println(result);
 }
