353a9e9c8bad9b72a81e1962028bf247b6fc1c5e
Pokita <84108412+Pipodestroyer@users.noreply.github.com>
2026-08-17 14:29:39 -0500

    i like to space my code because i cant read

--- untitled/src/Challenge3.java
+++ untitled/src/Challenge3.java
@@ -1,33 +1,42 @@
 public static String shortestPalindrome(String s) {
+
     String[] dividido = s.split("(?=.$)");
     String[] listatotal = dividido[0].split("");
     int largoTotal = listatotal.length-1;
 
     for (int i = 0; i <= listatotal.length-1; i++) {
+
         if(listatotal.length % 2 == 0) {
+
             if (listatotal.length / 2 == i) {
                 break;
             }
+
             String temp = listatotal[i];
             String temp2 = listatotal[largoTotal];
 
             listatotal[largoTotal] = temp;
             listatotal[i] = temp2;
             largoTotal = largoTotal-1;
+
         } else {
+
             if (listatotal.length-1 == i) {
                 break;
             }
+
             String temp = listatotal[i];
             String temp2 = listatotal[largoTotal];
 
             listatotal[largoTotal] = temp;
             listatotal[i] = temp2;
             largoTotal = largoTotal-1;
+
         }
     }
 
     return s + String.join("", listatotal);
+
 }
 
 void main(){
@@ -35,4 +44,5 @@ void main(){
     String result = shortestPalindrome("abbc");
 
     IO.println(result);
+
 }
